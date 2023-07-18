f06a4891365d11894720f390633e89cbf32b7451
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-18 13:36:38 +0200

    perimeter and area calculated

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,4 +1,28 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("square misures");
+        System.out.println("base misure");
+        int baseValue = Integer.parseInt(scan.nextLine());
+
+        System.out.println("high misure");
+        int highvalue = Integer.parseInt(scan.nextLine());
+
+        //creo un oggetto square
+
+        Squares1 square = new Squares1(baseValue, highvalue);
+
+        //chiedo all'impiegato di calcolare area e perimetro
+
+        System.out.println("Square perimeter:" + square.getsquareperimeter());
+
+        System.out.println("Square area:" + square.getsquarearea());
+
+
+    }
 }
